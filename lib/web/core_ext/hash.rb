473325dfd4ed73b79d9aefc65c3dc6e9436a75c2
John Nunemaker <nunemaker@gmail.com>
2008-07-27 18:15:33 -0400

    added a bit to hash conversion so that id does not throw deprecation warnings

--- lib/web/core_ext/hash.rb
+++ lib/web/core_ext/hash.rb
@@ -4,7 +4,13 @@ module Web
       def to_struct
         o = OpenStruct.new
         self.each do |k, v|
-          o.send("#{k}=", v.is_a?(Hash) ? v.to_struct : v)
+          # if id, we create an accessor so we don't get warning about id deprecation
+          if k.to_s == 'id'
+            o.class.class_eval "attr_accessor :id"
+            o.id = v
+          else
+            o.send("#{k}=", v.is_a?(Hash) ? v.to_struct : v)
+          end
         end
         o
       end
