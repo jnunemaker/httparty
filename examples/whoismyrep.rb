f8ce6cd36e429da204f44050c5b9f49977a2dfcc
John Nunemaker <nunemaker@gmail.com>
2008-07-28 15:47:17 -0400

    added most basic api consuming example

---
+++ examples/whoismyrep.rb
@@ -0,0 +1,10 @@
+dir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
+require File.join(dir, 'httparty')
+require 'pp'
+
+class Rep
+  include HTTParty
+  format :xml
+end
+
+puts Rep.get('http://whoismyrepresentative.com/whoismyrep.php?zip=46544').inspect
