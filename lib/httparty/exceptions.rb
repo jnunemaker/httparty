e999ce11af86fcbf56d6557eeef1439386dcedeb
John Nunemaker <nunemaker@gmail.com>
2009-01-30 23:23:47 -0500

    Added docs for exceptions and moved rexml fix into core extensions from /bin/httparty

--- lib/httparty/exceptions.rb
+++ lib/httparty/exceptions.rb
@@ -1,4 +1,7 @@
 module HTTParty
+  # Exception raised when you attempt to set a non-existant format
   class UnsupportedFormat < StandardError; end
+  
+  # Exception that is raised when request has redirected too many times
   class RedirectionTooDeep < StandardError; end
 end
