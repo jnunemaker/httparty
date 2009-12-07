67ff0e939921bf3f56818cd5ccb55c09c5e95f16
Sandro Turriate <sandro.turriate@gmail.com>
2009-12-07 00:46:05 -0500

    Add documentation to parser

--- lib/httparty/parser.rb
+++ lib/httparty/parser.rb
@@ -1,4 +1,41 @@
 module HTTParty
+  # The default parser used by HTTParty, supports xml, json, html, yaml, and
+  # plain text.
+  #
+  # == Custom Parsers
+  #
+  # If you'd like to do your own custom parsing, subclassing HTTParty::Parser
+  # will make that process much easier. There are a few different ways you can
+  # utilize HTTParty::Parser as a superclass.
+  #
+  # @example Intercept the parsing for all formats
+  #   class SimpleParser < HTTParty::Parser
+  #     def parse
+  #       perform_parsing
+  #     end
+  #   end
+  #
+  # @example Add the atom format and parsing method to the default parser
+  #   class AtomParsingIncluded < HTTParty::Parser
+  #     SupportedFormats.merge!(
+  #       {"application/atom+xml" => :atom}
+  #     )
+  #
+  #     def atom
+  #       perform_atom_parsing
+  #     end
+  #   end
+  #
+  # @example Only support the atom format
+  #   class ParseOnlyAtom < HTTParty::Parser
+  #     SupportedFormats = {"application/atom+xml" => :atom}
+  #
+  #     def atom
+  #       perform_atom_parsing
+  #     end
+  #   end
+  #
+  # @abstract Read the Custom Parsers section for more information.
   class Parser
     SupportedFormats = {
       'text/xml'               => :xml,
@@ -13,24 +50,40 @@ module HTTParty
       'text/plain'             => :plain
     }
 
-    attr_reader :body, :format
+    # The response body of the request
+    # @return [String]
+    attr_reader :body
 
+    # The intended parsing format for the request
+    # @return [Symbol] e.g. :json
+    attr_reader :format
+
+    # @param [String] body the response body
+    # @param [Symbol] format the response format
+    # @return parsed response
     def self.call(body, format)
       new(body, format).parse
     end
 
+    # @return [Hash] the SupportedFormats hash
     def self.formats
       const_get(:SupportedFormats)
     end
 
+    # @param [String] mimetype response MIME type
+    # @return [Symbol]
+    # @return [nil] mime type not supported
     def self.format_from_mimetype(mimetype)
       formats[formats.keys.detect {|k| mimetype.include?(k)}]
     end
 
+    # @return [Array<Symbol>] list of supported formats
     def self.supported_formats
       formats.values.uniq
     end
 
+    # @param [Symbol] format e.g. :json, :xml
+    # @return [Boolean]
     def self.supports_format?(format)
       supported_formats.include?(format)
     end
@@ -41,6 +94,8 @@ module HTTParty
     end
     private_class_method :new
 
+    # @return [Object] the parsed body
+    # @return [nil] when the response body is nil or an empty string
     def parse
       return nil if body.nil? || body.empty?
       if supports_format?
