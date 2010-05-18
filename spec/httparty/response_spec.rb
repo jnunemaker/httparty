da145a7ffd97e904ed7b3a484c0e92549bb72da2
Sandro Turriate <sandro.turriate@gmail.com>
2010-05-18 13:40:36 -0400

    Whitespace

--- spec/httparty/response_spec.rb
+++ spec/httparty/response_spec.rb
@@ -9,15 +9,15 @@ describe HTTParty::Response do
       @message = 'OK'
       @response = HTTParty::Response.new(@response_object, @body, @code, @message)
     end
-    
+
     it "should set delegate" do
       @response.delegate.should == @response_object
     end
-    
+
     it "should set body" do
       @response.body.should == @body
     end
-    
+
     it "should set code" do
       @response.code.should.to_s == @code
     end
@@ -25,22 +25,22 @@ describe HTTParty::Response do
     it "should set code as a Fixnum" do
       @response.code.should be_an_instance_of(Fixnum)
     end
-    
+
     it "should set body" do
       @response.body.should == @body
     end
   end
-  
+
   it "should be able to set headers during initialization" do
     response = HTTParty::Response.new({'foo' => 'bar'}, "{foo:'bar'}", 200, 'OK', {'foo' => 'bar'})
     response.headers.should == {'foo' => 'bar'}
   end
-  
+
   it "should send missing methods to delegate" do
     response = HTTParty::Response.new({'foo' => 'bar'}, "{foo:'bar'}", 200, 'OK')
     response['foo'].should == 'bar'
   end
-  
+
   it "should be able to iterate delegate if it is array" do
     response = HTTParty::Response.new([{'foo' => 'bar'}, {'foo' => 'baz'}], "[{foo:'bar'}, {foo:'baz'}]", 200, 'OK')
     response.size.should == 2
@@ -48,12 +48,12 @@ describe HTTParty::Response do
       response.each { |item| }
     }.should_not raise_error
   end
-  
+
   xit "should allow hashes to be accessed with dot notation" do
     response = HTTParty::Response.new({'foo' => 'bar'}, "{foo:'bar'}", 200, 'OK')
     response.foo.should == 'bar'
   end
-  
+
   xit "should allow nested hashes to be accessed with dot notation" do
     response = HTTParty::Response.new({'foo' => {'bar' => 'baz'}}, "{foo: {bar:'baz'}}", 200, 'OK')
     response.foo.should == {'bar' => 'baz'}
