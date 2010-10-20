21a457599b42f5ddbc4548995ed6a7351aba1a4f
Sandro Turriate <sandro.turriate@gmail.com>
2010-10-20 09:48:54 -0400

    Specfix: Signed certs don't expire after 30 days

--- spec/fixtures/ssl/generate.sh
+++ spec/fixtures/ssl/generate.sh
@@ -21,8 +21,8 @@ openssl req -batch -subj '/CN=localhost' -newkey rsa:1024 -new -x509 -days 99999
 openssl req -batch -subj '/CN=bogo' -new -days 999999 -key generated/server.key -nodes -out generated/bogushost.csr
 
 # Sign the certificate requests
-openssl x509 -CA generated/ca.crt -CAkey generated/ca.key -set_serial 1 -in generated/selfsigned.crt -out generated/server.crt -clrext -extfile openssl-exts.cnf -extensions cert
-openssl x509 -req -CA generated/ca.crt -CAkey generated/ca.key -set_serial 1 -in generated/bogushost.csr -out generated/bogushost.crt -clrext -extfile openssl-exts.cnf -extensions cert
+openssl x509 -CA generated/ca.crt -CAkey generated/ca.key -set_serial 1 -in generated/selfsigned.crt -out generated/server.crt -clrext -extfile openssl-exts.cnf -extensions cert -days 999999
+openssl x509 -req -CA generated/ca.crt -CAkey generated/ca.key -set_serial 1 -in generated/bogushost.csr -out generated/bogushost.crt -clrext -extfile openssl-exts.cnf -extensions cert -days 999999
 
 # Remove certificate signing requests
 rm -f generated/*.csr
