71d6455a049b3fae5fe498c9bf9f51fa3129d0a7
Sandro Turriate <sandro.turriate@gmail.com>
2011-01-18 15:37:12 -0500

    Example of website sign in with cookies

---
+++ examples/tripit_sign_in.rb
@@ -0,0 +1,33 @@
+dir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
+require File.join(dir, 'httparty')
+
+class TripIt
+  include HTTParty
+  base_uri 'http://www.tripit.com'
+  debug_output
+
+  def initialize(email, password)
+    @email = email
+    response = self.class.get('/account/login')
+    response = self.class.post(
+      '/account/login',
+      :body => {
+        :login_email_address => email,
+        :login_password => password
+      },
+      :headers => {'Cookie' => response.headers['Set-Cookie']}
+    )
+    @cookie = response.request.options[:headers]['Cookie']
+  end
+
+  def account_settings
+    self.class.get('/account/edit', :headers => {'Cookie' => @cookie})
+  end
+
+  def logged_in?
+    account_settings.include? "You're logged in as #{@email}"
+  end
+end
+
+tripit = TripIt.new('email', 'password')
+puts "Logged in: #{tripit.logged_in?}"
