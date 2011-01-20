781b3fc1e8c1da419f4e7d04b96791ed1d09f9c5
Björn Albers <bjoernalbers@googlemail.com>
2011-01-20 01:16:57 +0100

    Fix digest auth for unspecified quality of protection (qop)

--- lib/httparty/net_digest_auth.rb
+++ lib/httparty/net_digest_auth.rb
@@ -3,33 +3,69 @@ require 'net/http'
 
 module Net
   module HTTPHeader
-    def digest_auth(user, password, response)
-      response['www-authenticate'] =~ /^(\w+) (.*)/
+    def digest_auth(username, password, response)
+      @header['Authorization'] = DigestAuthenticator.new(username, password,
+          @method, @path, response).authorization_header
+    end
+    
+    
+    class DigestAuthenticator     
+      def initialize(username, password, method, path, response_header)
+        @username = username
+        @password = password
+        @method   = method
+        @path     = path
+        @response = parse(response_header)
+      end
+      
+      def authorization_header
+        @cnonce = md5(random)
+        header = [%Q(Digest username="#{@username}"),
+          %Q(realm="#{@response['realm']}"),
+          %Q(nonce="#{@response['nonce']}"),
+          %Q(uri="#{@path}"),
+          %Q(response="#{request_digest}")]
+        [%Q(cnonce="#{@cnonce}"),
+          %Q(opaque="#{@response['opaque']}"),
+          %Q(qop="#{@response['qop']}"),
+          %Q(nc="0")].each { |field| header << field } if qop_present?
+        header
+      end
+      
+    private
+    
+      def parse(response_header)
+        response_header['www-authenticate'] =~ /^(\w+) (.*)/
+        params = {}
+        $2.gsub(/(\w+)="(.*?)"/) { params[$1] = $2 }
+        params
+      end
+    
+      def qop_present?
+        @response.has_key?('qop') and not @response['qop'].empty?
+      end
+      
+      def random
+        "%x" % (Time.now.to_i + rand(65535))
+      end
+      
+      def request_digest
+        a = [md5(a1), @response['nonce'], md5(a2)]
+        a.insert(2, "0", @cnonce, @response['qop']) if qop_present?
+        md5(a.join(":"))
+      end
+      
+      def md5(str)
+        Digest::MD5.hexdigest(str)
+      end
+      
+      def a1
+        [@username, @response['realm'], @password].join(":")
+      end
 
-      params = {}
-      $2.gsub(/(\w+)="(.*?)"/) { params[$1] = $2 }
-      params.merge!("cnonce" => Digest::MD5.hexdigest("%x" % (Time.now.to_i + rand(65535))))
-
-      a_1 = Digest::MD5.hexdigest("#{user}:#{params['realm']}:#{password}")
-      a_2 = Digest::MD5.hexdigest("#{@method}:#{@path}")
-
-      request_digest = Digest::MD5.hexdigest(
-        [a_1, params['nonce'], "0", params['cnonce'], params['qop'], a_2].join(":")
-      )
-
-      header = [
-        %Q(Digest username="#{user}"),
-        %Q(realm="#{params['realm']}"),
-        %Q(qop="#{params['qop']}"),
-        %Q(uri="#{@path}"),
-        %Q(nonce="#{params['nonce']}"),
-        %Q(nc="0"),
-        %Q(cnonce="#{params['cnonce']}"),
-        %Q(opaque="#{params['opaque']}"),
-        %Q(response="#{request_digest}")
-      ]
-
-      @header['Authorization'] = header
+      def a2
+        [@method, @path].join(":")
+      end
     end
   end
 end
