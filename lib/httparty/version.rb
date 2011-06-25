0ae3183a21a44900209a86c47ba542007e5b26cd
Danny Tran <danny@danny-trans-macbook-pro.local>
2011-06-25 13:49:54 -0500

    Refreezing version strings.

--- lib/httparty/version.rb
+++ lib/httparty/version.rb
@@ -1,6 +1,6 @@
 module HTTParty
-  VERSION = "0.7.8"
+  VERSION = "0.7.8".freeze
 
-  CRACK_DEPENDENCY = "0.1.8"
-  SOCKSIFY_DEPENDENCY = "1.4.1"
+  CRACK_DEPENDENCY = "0.1.8".freeze
+  SOCKSIFY_DEPENDENCY = "1.4.1".freeze
 end
