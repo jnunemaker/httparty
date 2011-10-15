4ccc7bf57cb03c7cfbc5d55e3bf8fffb94453884
John Nunemaker <nunemaker@gmail.com>
2011-10-15 14:20:12 -0400

    Added nokogiri html parser example.

---
+++ examples/nokogiri_html_parser.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+require 'nokogiri'
+
+
+dir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
+require File.join(dir, 'httparty')
+require 'pp'
+
+class HtmlParserIncluded < HTTParty::Parser
+  SupportedFormats.merge!('text/html' => :html)
+
+  def html
+    Nokogiri::HTML(body)
+  end
+end
+
+class Page
+  include HTTParty
+  parser HtmlParserIncluded
+end
+
+pp Page.get('http://www.google.com')
