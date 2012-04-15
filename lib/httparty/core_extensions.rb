274eff85a2f01a31432cdfef451512ecb2cbac52
Isaac Sanders <isaacbfsanders@gmail.com>
2012-04-15 13:09:51 -0500

    Added Net::HTTP::Patch implmentation to core_exts

--- lib/httparty/core_extensions.rb
+++ lib/httparty/core_extensions.rb
@@ -6,4 +6,27 @@ module HTTParty
       instance_methods.each { |m| undef_method m unless m =~ /^__|instance_eval/ }
     end
   end
+
+  unless defined?(Net::HTTP::Patch)
+    class Net::HTTP
+      def patch(path, data, initheader = nil, dest = nil, &block) #:nodoc:
+        res = nil
+        request(Patch.new(path, initheader), data) {|r|
+          r.read_body dest, &block
+          res = r
+        }
+        unless @newimpl
+          res.value
+          return res, res.body
+        end
+        res
+      end
+
+      class Patch < Net::HTTPRequest
+        METHOD = 'PATCH'
+        REQUEST_HAS_BODY = true
+        RESPONSE_HAS_BODY = true
+      end
+    end
+  end
 end
