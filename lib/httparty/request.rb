88813cf36daa8952b56bd75b870666061bdae316
Neil Bourgeois <neil.bourgeois@gmail.com>
2012-11-11 12:23:02 -0700

    follow redirects to relative locations containing parent or current directories

--- lib/httparty/request.rb
+++ lib/httparty/request.rb
@@ -162,9 +162,9 @@ module HTTParty
     end
 
     def handle_response(body)
-      if response_redirects?
-        options[:limit] -= 1
-        self.path = last_response['location']
+      if response_redirects?                      
+        options[:limit] -= 1        
+        self.path = URI.parse(last_response['location']).relative? ? @last_uri.merge(last_response['location']).to_s : last_response['location']
         self.redirect = true
         self.http_method = Net::HTTP::Get unless options[:maintain_method_across_redirects]
         capture_cookies(last_response)
