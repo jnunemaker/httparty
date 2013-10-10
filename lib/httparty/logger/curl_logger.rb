98f53cfb4f1279d5edd14a3fe42a0a94742fc1dc
John Nunemaker <nunemaker@gmail.com>
2013-10-10 11:24:59 -0400

    Get rid of ivar and use local/param.

--- lib/httparty/logger/curl_logger.rb
+++ lib/httparty/logger/curl_logger.rb
@@ -12,41 +12,41 @@ module HTTParty
 
       def format(request, response)
         @messages       = []
-        @current_time   = Time.new.strftime("%Y-%m-%d %H:%M:%S %z")
+        time            = Time.now.strftime("%Y-%m-%d %H:%M:%S %z")
         http_method     = request.http_method.name.split("::").last.upcase
         path            = request.path.to_s
 
-        print_outgoing "#{http_method} #{path}"
+        print_outgoing time, "#{http_method} #{path}"
         if request.options[:headers] && request.options[:headers].size > 0
           request.options[:headers].each do |k, v|
-            print_outgoing "#{k}: #{v}"
+            print_outgoing time, "#{k}: #{v}"
           end
         end
 
-        print_outgoing request.raw_body
-        print_outgoing ""
-        print_incoming "HTTP/#{response.http_version} #{response.code}"
+        print_outgoing time, request.raw_body
+        print_outgoing time, ""
+        print_incoming time, "HTTP/#{response.http_version} #{response.code}"
 
         headers = response.respond_to?(:headers) ? response.headers : response
         response.each_header do |response_header|
-          print_incoming "#{response_header.capitalize}: #{headers[response_header]}"
+          print_incoming time, "#{response_header.capitalize}: #{headers[response_header]}"
         end
 
-        print_incoming "\n#{response.body}"
+        print_incoming time, "\n#{response.body}"
 
         @logger.send @level, @messages.join("\n")
       end
 
-      def print_outgoing(line)
-        @messages << print(">", line)
+      def print_outgoing(time, line)
+        @messages << print(time, ">", line)
       end
 
-      def print_incoming(line)
-        @messages << print("<", line)
+      def print_incoming(time, line)
+        @messages << print(time, "<", line)
       end
 
-      def print(direction, line)
-        "[#{TAG_NAME}] [#{@current_time}] #{direction} #{line}"
+      def print(time, direction, line)
+        "[#{TAG_NAME}] [#{time}] #{direction} #{line}"
       end
     end
   end
