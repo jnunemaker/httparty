44213083695aa9a53932db2b2f2d3613485c234e
Lucas Uyezu <lucas.uyezu@gmail.com>
2014-03-06 16:54:10 -0300

    Redirects and final responses handles Content-Length differently

--- lib/httparty/logger/apache_logger.rb
+++ lib/httparty/logger/apache_logger.rb
@@ -14,7 +14,7 @@ module HTTParty
         current_time   = Time.now.strftime("%Y-%m-%d %H:%M:%S %z")
         http_method    = request.http_method.name.split("::").last.upcase
         path           = request.path.to_s
-        content_length = response['Content-Length']
+        content_length = response.respond_to?(:headers) ? response.headers['Content-Length'] : response['Content-Length']
         @logger.send @level, "[#{TAG_NAME}] [#{current_time}] #{response.code} \"#{http_method} #{path}\" #{content_length || "-"} "
       end
     end
