19ab7ab4d45093ff68611ef99029f71b98f3cf06
Thomas Nys <nysthee@gmail.com>
2015-04-13 23:12:55 +0200

    Mitigates Lint/StringConversionInInterpolation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'httparty'
 require 'fakeweb'
 
 def file_fixture(filename)
-  open(File.join(File.dirname(__FILE__), 'fixtures', "#{filename.to_s}")).read
+  open(File.join(File.dirname(__FILE__), 'fixtures', "#{filename}")).read
 end
 
 Dir[File.expand_path(File.join(File.dirname(__FILE__), 'support', '**', '*.rb'))].each { |f| require f }
